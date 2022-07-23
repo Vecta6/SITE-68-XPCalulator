c044407aecdfb9065ccfdd9307cd298f81ce921a
Vecta_ <59992068+Vecta6@users.noreply.github.com>
2022-07-23 20:10:54 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -6,13 +6,13 @@ function prossess() {
     var Goal = document.getElementById("Goal").value;
     console.log(MyXP + " " + typeof(MyXP));
     console.log(Goal + " " + typeof(Goal));
-    if (Goal == Emp) {
+    if (Goal == 0) {
         document.getElementById("result").innerHTML = 'Attention, il faut obligatoirement mettre quelque chose dans la partie en <em class="emred">ROUGE</em>';
     } else {
         value = ((Goal - MyXP) / 10) / 60;
         valuesplit = String(value).split(".");
         valuehour = Number(valuesplit[0]);
-        valuemin = Number("0." + valueslplit[1]) * 60;
+        valuemin = Number("0." + valuesplit[1]) * 60;
         valuemin = Math.round(valuemin);
         document.getElementById("result").innerHTML = 'Il vous reste encore <em class="emunderline">' + valuehour + "h" + valuemin + '</em> ∓ 5min de jeu pour atteidre votre but';
     }
