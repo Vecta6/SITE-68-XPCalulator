1f3729d92cc4144008b17564b0b8d6e809233c47
Vecta_ <59992068+Vecta6@users.noreply.github.com>
2022-07-23 19:53:02 +0200

    Add files via upload

---
+++ js/animations.js
@@ -0,0 +1,46 @@
+anim = false
+
+if (anim) {
+    setInterval(function() {
+        setTimeout(async function() {
+            document.title = "S";
+            setTimeout(function() {
+                document.title = "SI";
+                setTimeout(function() {
+                    document.title = "SIT";
+                    setTimeout(function() {
+                        document.title = "SITE";
+                        setTimeout(function() {
+                            document.title = "SITE -";
+                            setTimeout(function() {
+                                document.title = "SITE - 6";
+                                setTimeout(function() {
+                                    document.title = "SITE - 68";
+                                    setTimeout(function() {
+                                        document.title = "ㅤ";
+                                        setTimeout(function() {
+                                            document.title = "SITE - 68";
+                                            setTimeout(function() {
+                                                document.title = "ㅤ";
+                                                setTimeout(function() {
+                                                    document.title = "SITE - 68";
+                                                    setTimeout(function() {
+                                                        document.title = "ㅤ";
+                                                        setTimeout(function() {
+                                                            document.title = "SITE - 68";
+                                                        }, 600)
+                                                    }, 300)
+                                                }, 300)
+                                            }, 300)
+                                        }, 300)
+                                    }, 300)
+                                }, 300)
+                            }, 300)
+                        }, 300)
+                    }, 300)
+                }, 300)
+            }, 300);
+        }, 300);
+    }, 4200)
+
+}
