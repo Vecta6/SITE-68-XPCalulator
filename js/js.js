52917582f9233effbcded6f88dbc61a7ea3da4e1
Vecta_ <59992068+Vecta6@users.noreply.github.com>
2022-07-23 19:52:48 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -6,19 +6,19 @@ function prossess() {
     var Goal = document.getElementById("Goal").value;
     console.log(MyXP + " " + typeof(MyXP));
     console.log(Goal + " " + typeof(Goal));
-    if (Goal == 0) {
+    if (Goal == Emp) {
         document.getElementById("result").innerHTML = 'Attention, il faut obligatoirement mettre quelque chose dans la partie en <em class="emred">ROUGE</em>';
     } else {
         value = ((Goal - MyXP) / 10) / 60;
         valuesplit = String(value).split(".");
         valuehour = Number(valuesplit[0]);
-        valuemin = Number("0." + valuesplit[1]) * 60;
+        valuemin = Number("0." + valueslplit[1]) * 60;
         valuemin = Math.round(valuemin);
         document.getElementById("result").innerHTML = 'Il vous reste encore <em class="emunderline">' + valuehour + "h" + valuemin + '</em> ∓ 5min de jeu pour atteidre votre but';
     }
 }
 
-function closepopup() {
+async function closepopup() {
     document.getElementById("popup").style.top = "-300px";
     document.getElementById("blur").style.visibility = "hidden";
     document.documentElement.style.overflow = "visible";
